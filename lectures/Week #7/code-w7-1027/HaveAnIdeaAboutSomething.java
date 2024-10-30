0f8bf70755531d6a1078e13dd1145e4d26974fe7
a24ibrah <a24ibrah@users.noreply.github.com>
2024-10-29 22:24:38 -0400

    !

--- lectures/Week #7/code-w7-1027/HaveAnIdeaAboutSomething.java
+++
@@ -1,6 +0,0 @@
-public class HaveAnIdeaAboutSomething{ 
-    public static void main(String ... args){ m1(); }
-public static void m1(){ m2(); }
-public static void m2(){ System.out.println("I Like 7"); }
-}
-
